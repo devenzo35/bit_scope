d202a4bad5e67e493b210a9ae6a9b1b3a5e73a18
Enzo <enzocuellar12@gmail.com>
2025-12-03 14:48:07 -0300

    plotly charts added to represent reddit sentiment data, UI is ready  for production

--- visualization/reddit.py
+++ visualization/reddit.py
@@ -1,6 +1,7 @@
 from storage.connection import get_db_connection
 import streamlit as st
 import pandas as pd
+from plotly import express as px
 
 # Use a pipeline as a high-level helper
 from transformers import pipeline
@@ -26,7 +27,7 @@ def reddit_page():
     query = "SELECT * FROM reddit_posts"
     reddit_df = con.execute(query).df()
 
-    reddit_df = reddit_df.head(15)
+    # reddit_df = reddit_df.head(15)
 
     reddit_df["selftext"] = reddit_df["selftext"].fillna("")
     reddit_df["combined_text"] = reddit_df["title"] + " " + reddit_df["selftext"]
@@ -38,7 +39,6 @@ def reddit_page():
     total_neutral_score = []
 
     for result in results:
-        st.write(result)
         if result["label"] == "positive":
             total_positive_score.append(result["score"])
         if result["label"] == "negative":
@@ -52,31 +52,88 @@ def reddit_page():
 
     total_score = avg_positive_score - avg_negative_score
 
-    st.metric(label="Average Positive Score", value=f"{avg_positive_score:4f}")
-    st.metric(label="Average Negative Score", value=f"{avg_negative_score:4f}")
-    st.metric(label="Average Neutral Score", value=f"{avg_neutral_score:4f}")
-
-    st.metric(label="Total Positive Posts", value=f"{len(total_positive_score)}")
-    st.metric(label="Total Negative Posts", value=f"{len(total_negative_score)}")
-    st.metric(label="Total Neutral Posts", value=f"{len(total_neutral_score)}")
+    st.title("Reddit Posts Sentiment Analysis")
 
     if (
         avg_neutral_score > avg_positive_score
         and avg_neutral_score > avg_negative_score
     ):
+        st.metric(label="Neutral Sentiment Score", value=f"{avg_neutral_score:4f}")
         st.warning("Overall Sentiment: Neutral")
-        st.metric(label="Total Sentiment Score", value=f"{avg_neutral_score:4f}")
     elif avg_negative_score > avg_positive_score:
+        st.metric(label="Total Sentiment Score", value=f"{total_score:4f}")
         if avg_negative_score > 0.3 and avg_negative_score <= 0.5:
-            st.warning("Warning:Very High Negative Sentiment Detected!")
+            st.error("Negative Sentiment Detected!")
         if avg_negative_score > 0.5 and avg_negative_score <= 0.7:
-            st.error("Alert: High Negative Sentiment Detected!")
+            st.error("High Negative Sentiment Detected!")
         if avg_negative_score > 0.7:
-            st.error("Critical Alert: Extremely High Negative Sentiment Detected!")
-        st.metric(label="Total Sentiment Score", value=f"{total_score:4f}")
+            st.error(" Extremely High Negative Sentiment Detected!")
     elif avg_positive_score > avg_negative_score:
-        st.header("Overall Sentiment: Positive")
         st.metric(label="Total Sentiment Score", value=f"{total_score:4f}")
+        if avg_positive_score > 0.3 and avg_positive_score <= 0.5:
+            st.success("Positive Sentiment Detected!")
+        if avg_positive_score > 0.5 and avg_positive_score <= 0.7:
+            st.success("Very High Positive Sentiment Detected!")
+        if avg_positive_score > 0.7:
+            st.success("Extremely High Positive Sentiment Detected!")
+
+    st.header("Average Sentiment")
+
+    avg_col1, avg_col2, avg_col3 = st.columns(3)
+
+    with avg_col1:
+        st.metric(label="Average Positive Score", value=f"{avg_positive_score:1f}")
+    with avg_col2:
+        st.metric(label="Average Negative Score", value=f"{avg_negative_score:2f}")
+    with avg_col3:
+        st.metric(label="Average Neutral Score", value=f"{avg_neutral_score:2f}")
+
+    fig = px.bar(
+        x=["Positive", "Negative", "Neutral"],
+        y=[
+            avg_positive_score,
+            avg_negative_score,
+            avg_neutral_score,
+        ],
+        labels={"x": "", "y": "Average Sentiment Score"},
+        color=["Positive", "Negative", "Neutral"],
+        color_discrete_map={
+            "Positive": "#7DCFB6",
+            "Negative": "#E63946",
+            "Neutral": "#D4B483",
+        },
+    )
+
+    st.plotly_chart(fig)
+
+    st.header("Total Posts divided by Sentiment")
+
+    total_col1, total_col2, total_col3 = st.columns(3)
+
+    with total_col1:
+        st.metric(label="Total Positive Posts", value=f"{len(total_positive_score)}")
+    with total_col2:
+        st.metric(label="Total Negative Posts", value=f"{len(total_negative_score)}")
+    with total_col3:
+        st.metric(label="Total Neutral Posts", value=f"{len(total_neutral_score)}")
+
+    fig2 = px.bar(
+        y=[
+            len(total_positive_score),
+            len(total_negative_score),
+            len(total_neutral_score),
+        ],
+        x=["Positive", "Negative", "Neutral"],
+        color=["Positive", "Negative", "Neutral"],
+        color_discrete_map={
+            "Positive": "#7DCFB6",
+            "Negative": "#E63946",
+            "Neutral": "#D4B483",
+        },
+        labels={"x": "", "y": "Total Posts"},
+    )
+
+    st.plotly_chart(fig2)
 
 
 if __name__ == "__main__":
